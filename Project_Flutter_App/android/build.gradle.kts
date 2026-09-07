9b7f982e2a59fb7f531fe256fce59d6f0690e42e
Hassan1566 <sh456700992sj@gmail.com>
2026-09-07 12:38:43 +0500

    feat: configure Android build system with Kotlin DSL and add VS Code Java settings

--- Project_Flutter_App/android/build.gradle.kts
+++ Project_Flutter_App/android/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.android.application") version "9.0.1" apply false
-    id("com.android.library") version "9.0.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.20" apply false
 }
 
