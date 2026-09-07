1cb2152454c9685b9c8e45c82e809677954cb822
Hassan1566 <sh456700992sj@gmail.com>
2026-09-07 12:52:58 +0500

    feat: create App widget and set LocationList as the home screen

--- Project_Flutter_App/lib/app.dart
+++ Project_Flutter_App/lib/app.dart
@@ -1,13 +1,11 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 import 'location_list.dart';
 
-// make main() call this widget
 class App extends StatelessWidget {
-  // constructor
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LocationList());
+    return const MaterialApp(home: LocationList());
   }
 }
